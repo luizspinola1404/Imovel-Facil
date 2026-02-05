fa5900ebe68c3bb879fbfd0080eeec938e3e19c9
luizspinola1404 <lspinolagoncalves@gmail.com>
2026-02-05 10:54:17 -0300

    Refactor static route to serveStatic function
    Refactor static route to a function for serving static content.

--- server/static.ts
+++ server/static.ts
@@ -1,10 +1,5 @@
-import express, { Request, Response } from "express";
+import { Express } from "express";
 
-const router = express.Router();
-
-// Exemplo de rota estática corrigida
-router.get("/static", (_req: Request, res: Response) => {
-  res.send("Conteúdo estático funcionando!");
-});
-
-export default router;
+export function serveStatic(_app: Express) {
+  // vazio por enquanto
+}
