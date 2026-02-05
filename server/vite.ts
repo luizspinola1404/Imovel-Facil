c6381686a754e95a36f53b1753d1a816a40a7ad5
luizspinola1404 <lspinolagoncalves@gmail.com>
2026-02-05 11:24:21 -0300

    Modify setupVite function signature
    Refactor setupVite to accept Server and Express parameters.

--- server/vite.ts
+++ server/vite.ts
@@ -1,3 +1,9 @@
-export async function setupVite() {
- }
+import { Server } from "http";
+import { Express } from "express";
 
+export async function setupVite(
+  _server: Server,
+  _app: Express
+) {
+  // vazio no Render
+}
